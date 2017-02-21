0013dac467c8283775ee46b8b363dfc60f9c6916
zackart98 <zackart98@gmail.com>
2017-02-21 11:47:24 -0800

    Adjust trimming, offsets. Add turning methods.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeConstants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by HSSROBOTICS on 2/16/2017.
+ */
+
+public class OpModeConstants
+{
+    public static final double RIGHT_MOTOR_OFFSET = 0.0;   // The speed offset of the right motor
+    public static final double LEFT_MOTOR_OFFSET = 0.0;    // The speed offset of the left motor
+    public static final double SPEED_MULT = 0.4;           // Multiplier for the speed of both drive motors
+
+
+    public static final double ARM_MIN_POS = 0.0;          // The minimum arm position
+    public static final double ARM_MAX_POS = 0.8;          // The maximum arm position
+}
