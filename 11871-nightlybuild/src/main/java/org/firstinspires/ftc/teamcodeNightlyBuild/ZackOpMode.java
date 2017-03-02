58d042a5dc02ad643190a26a826a042a92ec4c53
agentx1118 <huston010@gmail.com>
2017-03-02 11:26:10 -0500

    Change armServo's setposition method call
    Removed the if statement and newArmPos variable in and attempt to reduce delay

--- 11871-nightlybuild/src/main/java/org/firstinspires/ftc/teamcodeNightlyBuild/ZackOpMode.java
+++ 11871-nightlybuild/src/main/java/org/firstinspires/ftc/teamcodeNightlyBuild/ZackOpMode.java
@@ -200,11 +200,7 @@ public class ZackOpMode extends LinearOpMode {
 
             // Use the left stick on controller 2 to move the arm
 
-            if(gamepad2.left_stick_y != 0.0)
-            {
-                newArmPos = robot.armServo.getPosition() + (.01*gamepad2.left_stick_y);
-                robot.armServo.setPosition(newArmPos);
-            }
+            robot.armServo.setPosition(robot.armServo.getPosition() + (.01*gamepad2.left_stick_y));
 
             // Send telemetry message to signify robot running
             // telemetry.addData("claw",  "Offset = %.2f", clawOffset);
