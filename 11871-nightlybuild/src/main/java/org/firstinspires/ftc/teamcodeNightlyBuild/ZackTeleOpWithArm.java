c2ac9964b078f17e629d3f486305636fd676b62a
agentx1118 <huston010@gmail.com>
2017-04-28 20:42:41 -0400

    Made a minor change in the group name

--- 11871-nightlybuild/src/main/java/org/firstinspires/ftc/teamcodeNightlyBuild/ZackTeleOpWithArm.java
+++ 11871-nightlybuild/src/main/java/org/firstinspires/ftc/teamcodeNightlyBuild/ZackTeleOpWithArm.java
@@ -53,7 +53,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name = "Arm Encoder", group = "South")
+@TeleOp(name = "Arm Encoder", group = "Pushbot")
 //@Disabled
 public class ZackTeleOpWithArm extends LinearOpMode {
 
